9810387e951790ebff157f577dec81cd775a570d
andrew <andrewasmit@hotmail.com>
2022-11-14 10:47:24 -0600

    commit tree 202967d9f0a471c898d7dc7231a9dc475a8cb9b1

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Polygon{
+    constructor(arr){
+        this.arr = arr;
+    }
+
+    get countSides(){
+        return this.arr.length;
+    }
+
+    get perimeter(){
+        return this.arr.reduce((previous, current)=>{
+          return  previous + current;
+        })
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        const a = this.arr[0];
+        const b = this.arr[1];
+        const c = this.arr[2];
+        if (a+b<c || a+c <b || b+c<a){
+            return false;
+        } else return true;  
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        const a = this.arr[0];
+        const b = this.arr[1];
+        const c = this.arr[2];
+        const d = this.arr[3];
+        if (a === b && a===c && a===d){
+            return true;
+        } else return false;
+    }
+
+    get area(){
+        return this.arr[0]*this.arr[0];
+    }
+}
